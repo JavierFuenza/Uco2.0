a6947051b444775c52504385818012ea2eb280f2
JavierFuenza <ja.fuenzalida@duocuc.cl>
2023-06-26 13:29:01 -0400

    funcionan paginas

--- pagina/urls.py
+++ pagina/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='Index'),
+    path('registro',views.registrar,name='registro'),
+    path('Nosotros',views.nosotros,name='Nosotros'),
+    path('pantalones',views.pantalones,name='pantalones'),
+    path('poleras',views.poleras,name='poleras'),
+    path('polerones',views.polerones,name='polerones'),
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context={}
-    return render(request, 'pagina/index.html', context)
+    return render(request, 'pagina/Index.html', context)
+def registrar(request):
+    context={}
+    return render(request, 'pagina/registro.html', context)
+def nosotros(request):
+    context={}
+    return render(request, 'pagina/Nosotros.html', context)
+def pantalones(request):
+    context={}
+    return render(request, 'pagina/pantalones.html', context)
+def poleras(request):
+    context={}
+    return render(request, 'pagina/poleras.html', context)
+def polerones(request):
+    context={}
+    return render(request, 'pagina/polerones.html', context)
